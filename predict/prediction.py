c9dea50febb9674c1410de1794780ca9a86a6b99
Jordi <74079212+bljordi78@users.noreply.github.com>
2025-07-07 12:34:17 +0200

    prediction folder update

--- predict/prediction.py
+++ predict/prediction.py
@@ -12,33 +12,3 @@ def predict(dataInput):
 
 
 
-#for testing purposes
-df = pd.read_csv("model/data_cleaned.csv")
-column_mapping = {
-    'haslift': 'lift',
-    'hasgarden': 'garden',
-    'hasswimmingpool': 'swimmingpool',
-    'hasterrace': 'terrace',
-    'hasparking': 'parking',
-    'epcscore_encoded': 'epcscore',
-    'buildingcondition_encoded': 'building_state',
-    'type_encoded': 'property_type'
-}
-
-df.rename(columns=column_mapping, inplace=True)
-
-print(df.iloc[2].value_counts())
-
-import random
-indexes = random.sample(range(len(df)), 10)
-for i in indexes:
-    dataInput = df.drop(columns='price').iloc[[i]]
-
-    dataOutput = predict(dataInput)
-    print(f"Predicted price: {dataOutput:.0f}")
-
-    # actual_price = df['price'].iloc[i]
-    # error = (dataOutput/actual_price - 1)  * 100
-    # print(f"Predicted price: {dataOutput:.0f}; Actual price: {actual_price:.0f}; Error: {error:.1f}%")
-
-    
